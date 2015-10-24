a766820a606479b72c7872492bce59b86cc76375
Francis Visoiu Mistrih <visoiu_f@epita.fr>
2015-10-25 01:08:47 +0200

    cleanup: remove init

--- include/mp2p/misc/all.hh
+++ include/mp2p/misc/all.hh
@@ -2,5 +2,4 @@
 
 #include <mp2p/misc/color.hh>
 #include <mp2p/misc/shared-buffer.hh>
-#include <mp2p/misc/init.hh>
 #include <mp2p/misc/logger.hh>
--- include/mp2p/misc/init.hh
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-namespace misc
-{
-  void check_system();
-} // namespace misc
